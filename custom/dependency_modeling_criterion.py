e8646d913f53de1ba58c2d1a7277e4547e5df7db
fadedcosine <873125553@qq.com>
2021-05-13 15:38:31 +0800

    add faster dep CE loss calculating

--- custom/dependency_modeling_criterion.py
+++ custom/dependency_modeling_criterion.py
@@ -38,33 +38,23 @@ class DependencyCrossEntropy(FairseqCriterion):
         lprobs = lprobs.view(-1, lprobs.size(-1))
         target = sample["target"]
         target = target.view(-1, target.size(-1))
-        # loss = 0
-        loss = torch.zeros(lprobs.size(0)).to(lprobs.device)
-        idx_range = torch.arange(target.size(-1)).to(lprobs.device)
-        
-        for i in range(lprobs.size(0)):
-            start_time = time.time()
-            dependency_num = torch.sum(target[i]).item()
-            dependency_num_time = time.time()
-            # print("dependency_num_time is : ", dependency_num_time - start_time)
-            if dependency_num > 0:
-                cur_logit = lprobs[i].unsqueeze(0)
-                cur_logit = cur_logit.expand(int(dependency_num), cur_logit.size(-1))
-                cur_logit_time = time.time()
-                # print("cur_logit_time is : ", cur_logit_time - dependency_num_time)
-                cur_target = idx_range.masked_select(target[i].bool())
-                cur_target_time = time.time()
-                # print("cur_target_time is : ", cur_target_time - cur_logit_time)
-                loss[i] = F.nll_loss(
-                    cur_logit,
-                    cur_target,
-                    ignore_index=self.padding_idx,
-                    reduction="sum" if reduce else "none",
-                )
-                nll_loss_time = time.time()
-                # print("nll_loss_time is : ", nll_loss_time - cur_target_time)
-        # print("return loss")
-        return torch.sum(loss)
+        # the following codes using for-loop to calculate loss are really slow
+        # loss = torch.zeros(lprobs.size(0)).to(lprobs.device)
+        # idx_range = torch.arange(target.size(-1)).to(lprobs.device)
+        # for i in range(lprobs.size(0)):
+        #     dependency_num = torch.sum(target[i]).item()
+        #     if dependency_num > 0:
+        #         cur_logit = lprobs[i].unsqueeze(0)
+        #         cur_logit = cur_logit.expand(int(dependency_num), cur_logit.size(-1))
+        #         cur_target = idx_range.masked_select(target[i].bool())
+        #         loss[i] = F.nll_loss(
+        #             cur_logit,
+        #             cur_target,
+        #             ignore_index=self.padding_idx,
+        #             reduction="sum" if reduce else "none",
+        #         )
+        loss = torch.sum(torch.bmm(torch.neg(lprobs).unsqueeze(1), target.unsqueeze(-1)))
+        return loss
     @staticmethod
     def reduce_metrics(logging_outputs) -> None:
         """Aggregate logging outputs from data parallel training."""
